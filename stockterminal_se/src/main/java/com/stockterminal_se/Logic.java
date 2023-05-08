a070f5023fd7d99caf70dd23c43bb96b29b66e2d
NoamBenS <noampvb@gmail.com>
2023-05-08 18:34:17 -0400

    Updates of 5/8

--- stockterminal_se/src/main/java/com/stockterminal_se/Logic.java
+++ stockterminal_se/src/main/java/com/stockterminal_se/Logic.java
@@ -1,5 +1,105 @@
 package com.stockterminal_se;
 
+import java.util.*;
+
 public class Logic {
+
+    private Data dataManager;
+    private Scanner argScanner;
+    private String[] args;
+    private char[] flags;
+    private String output;
     
+    public Logic() {
+        this.dataManager = new Data();
+        start(getArgs());
+    }
+
+    public Logic(String[] args) {
+        this.dataManager = new Data();
+        start(args);
+    }
+    
+    public static void main(String[] args) {
+        Logic logic = new Logic(args);
+    }
+
+    /**
+     * This method is called on startup to get the first args if they are not provided.
+     * @return String[] of args
+     */
+    private String[] getArgs() {
+        this.argScanner = new Scanner(System.in);
+        ArrayList<String> args = new ArrayList<>();
+        String in = this.argScanner.next();
+        while (in != null) {
+            args.add(in);
+            in = this.argScanner.next();
+        }
+        this.argScanner.close();
+        if (args.size() == 0) {
+            System.out.println("Input was empty, please try again. (If you need help, enter \"help\")");
+            return getArgs();
+        }
+        return (String[]) args.toArray();
+    }
+
+    /**
+     * This method starts the whole program once it has proper input.
+     */
+    private void start(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Input was empty, please try again. (If you need help, enter \"help\")");
+            args = getArgs();
+        }
+        this.args = filter(args);
+        this.flags = getFlags(this.args);
+        if (this.args == null || this.flags == null) {
+            continueProgram();
+        }
+
+        // finish
+    }
+
+    private String[] filter(String[] args) {
+        ArrayList<String> tickersAndFlags = new ArrayList<>();
+        boolean badInput = false;
+        for (int index = 0; index < args.length && !badInput; index++) {
+            if (args[index].matches("[a-z-]+")) {
+                if (args[index].matches("[a-z]+")) {
+                    if (args[index].length() > 0 && args[index].length() < 5) {
+                        tickersAndFlags.add(args[index]);
+                        if (index == args.length - 1)
+                            return (String[]) tickersAndFlags.toArray();
+                    } else
+                        badInput = true;
+                } else if (args[index].startsWith("-") && index == args.length - 1) {
+                    tickersAndFlags.add(args[index]);
+                    return (String[]) tickersAndFlags.toArray();
+                } else
+                    badInput = true;
+            }
+        }
+        System.out.println(
+                    "Input or part of input was invalid. Please try again. (If you need help, enter \"help\")");
+        return null;
+    }
+    
+    private char[] getFlags(String[] args) {
+        ArrayList<Character> flags = new ArrayList<>();
+        if (args[args.length - 1].startsWith("-") && args[args.length - 1].matches("[chloprtv]")) {
+            // finish
+        } else
+            return new char[1];
+    }
+
+    private void continueProgram() {
+        this.args = filter(getArgs());
+        this.flags = getFlags(this.args);
+        if (this.args == null || this.flags == null) {
+            continueProgram();
+        }
+
+        // finish
+    }
 }
--- stockterminal_se/src/main/java/com/stockterminal_se/Stock.java
+++ stockterminal_se/src/main/java/com/stockterminal_se/Stock.java
@@ -1,5 +1,156 @@
 package com.stockterminal_se;
 
-public class Stock {
+import java.io.*;
+import java.net.*;
+
+public class Stock implements Comparable<Stock> {
+
+    // all information, formatted
+    private String allData;
+
+    // values for individual information
+    private String ticker;
+    private String open;
+    private String high;
+    private String low;
+    private String price;
+    private String volume;
+    private String latestTradingDay;
+    private String prevClose;
+    private String changeNum;
+    private String changePercent;
+
+    //time value
+    private long lastUseTime;
     
+    public Stock (String ticker) {
+        this.ticker = ticker;
+    }
+
+    protected void refresh(String apiKey) throws IOException {
+        String rawData = makeCall(apiKey); // creates a String of ALL data
+        setVariables(rawData);
+        setUseTime();
+    }
+
+    // time-use methods and the compareTo associated with it
+    public void setUseTime() {
+        this.lastUseTime = System.currentTimeMillis();
+    }
+
+    public long getLastUseTime() {
+        return this.lastUseTime;
+    }
+
+    @Override
+    public int compareTo(Stock o) {
+        return (this.lastUseTime < o.getLastUseTime()) ? -1 : ((this.lastUseTime == o.getLastUseTime()) ? 0: 1);
+    }
+
+
+    // helper methods
+
+    private String makeCall(String apiKey) throws IOException {
+        String urlString = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + this.ticker + "&apikey=" + apiKey;
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        // creates request using stock information and sets the request
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        //creates a BfferedReader for the input stream of the get for information and updates content for it.
+
+        return content.toString();
+        //returns content as a String.
+    }
+
+    private void setVariables(String info) {
+        String[] separateAspects = info.split("\"");
+
+        /*
+        indexes are based on testing the split of information
+        received in call
+        */
+        this.ticker = separateAspects[5];
+        this.open = separateAspects[9];
+        this.high = separateAspects[13];
+        this.low = separateAspects[17];
+        this.price = separateAspects[21];
+        this.volume = separateAspects[25];
+        this.latestTradingDay = separateAspects[29];
+        this.prevClose = separateAspects[33];
+        this.changeNum = separateAspects[37];
+        this.changePercent = separateAspects[41];
+
+        createAllData(separateAspects);
+    }
+
+    private void createAllData(String[] separate) {
+        StringBuffer concatenated = new StringBuffer();
+        String title;
+        String core;
+        for (int i = 5; i < 42; i = i + 4) {
+            title = separate[i - 2].substring(4);
+            core = separate[i];
+			if (i == 5) {
+				concatenated.append(title + ": " + core);
+			}
+            else {
+            concatenated.append("\n" + title + ": " + core);
+			}
+        }
+        this.allData = concatenated.toString();
+    }
+
+    //getters
+    protected String allData() {
+        return this.allData;
+    }
+
+    protected String symbol() {
+        return this.ticker;
+    }
+
+    protected String open() {
+        return this.open;
+    }
+
+    protected String high() {
+        return this.high;
+    }
+
+    protected String low() {
+        return this.low;
+    }
+
+    protected String price() {
+        return this.price;
+    }
+
+    protected String volume() {
+        return this.volume;
+    }
+
+    protected String latestTrading() {
+        return this.latestTradingDay;
+    }
+
+    protected String prevClose() {
+        return this.prevClose;
+    }
+
+    protected String changeNum() {
+        return this.changeNum;
+    }
+
+    protected String changePercent() {
+        return this.changePercent;
+    }
+
 }
