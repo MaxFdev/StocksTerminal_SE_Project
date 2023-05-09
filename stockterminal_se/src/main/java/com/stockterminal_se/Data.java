0dfef1640ca94bde05e2a1b1a9f9c627de49f886
Max Franklin <mifrank1@mail.yu.edu>
2023-05-09 16:58:25 -0400

    Added Comments to Data

--- stockterminal_se/src/main/java/com/stockterminal_se/Data.java
+++ stockterminal_se/src/main/java/com/stockterminal_se/Data.java
@@ -30,40 +30,73 @@ public class Data {
         public int compare(Stock one, Stock two) {
             if (one.getLastUseTime() > two.getLastUseTime()) {
                 return 1;
-            }
-            else if (one.getLastUseTime() == two.getLastUseTime()) {
+            } else if (one.getLastUseTime() == two.getLastUseTime()) {
                 return 1;
             }
             return -1;
         }
     }
     
+    /**
+     * This method takes a ticker as a param and returns an updated stock object.
+     * 
+     * @param ticker
+     * @return
+     */
     public Stock query(String ticker) {
 
     }
     
+    /**
+     * This method is the same except it deals with multiple stocks.
+     * 
+     * @param tickers
+     * @return
+     */
     public List<Stock> query(String[] tickers) {
 
     }
     
+    /**
+     * This method clears all the stock objects out of data.
+     */
     public void clear() {
 
     }
     
+    /**
+     * This method refreshes all Stocks being held in the heap.
+     */
     public void refresh() {
 
     }
     
+    /**
+     * This method removes a specific stock from data.
+     * 
+     * @param ticker
+     * @return
+     */
     public boolean remove(String ticker) {
 
     }
     
+    /**
+     * Returns a list of all stocks ever called.
+     * 
+     * @return
+     */
     public List<Stock> history() {
 
     }
     
+    /**
+     * This method takes one stock that will be updated to provide live data.
+     * 
+     * @param ticker
+     * @return
+     */
     public Stock live(String ticker) {
         
     }
-
 }
